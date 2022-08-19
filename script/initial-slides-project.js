f9d82f9abd9708179b3c89fdd3667c6db7c1e91b
Konstantin Vavilov <gravekon111@yandex.ru>
2022-08-19 09:00:39 +0500

    feat: разработать секцию проект/слайдер

---
+++ script/initial-slides-project.js
@@ -0,0 +1,38 @@
+const initialSlidesProject = [
+  {
+    heading: 'Фотопроект «Семья как семья»',
+    text: 'Фотопроект Алексея Горшенина включает в себя восемь историй людей и семей, где один или оба супруга имеют инвалидность',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    link: '#'
+  },
+  {
+    heading: '2Lorem ipsum, dolor sit amet',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero dolores quia nam alias autem repellendus, repudiandae debitis est.',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    link: '#'
+  },
+  {
+    heading: '3Lorem ipsum, dolor sit amet',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero dolores quia nam alias autem repellendus, repudiandae debitis est.',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    link: '#'
+  },
+  {
+    heading: '4Lorem ipsum, dolor sit amet',
+    text: ' adipisicing elit. Vero dolores quia nam alias autem repellendus, repudiandae debitis est.',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    link: '#'
+  },
+  {
+    heading: '5Lorem ipsum, dolor sit amet',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repudiandae debitis est.',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    link: '#'
+  },
+  {
+    heading: '6Lorem ipsum, dolor sit amet',
+    text: 'Lorem ipsum, dolor sit amet consectetur  quia nam alias autem repellendus, repudiandae debitis est.',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    link: '#'
+  }
+];
---
+++ script/script-slider-project.js
@@ -0,0 +1,75 @@
+const slideProjectTemplate = document.querySelector('#slide-for-project').content;
+const buttonPrevSliderPrct = document.querySelector('.slider-project__button_type_prev');
+const buttonNextSliderPrct = document.querySelector('.slider-project__button_type_next');
+
+const sliderProjectContainer = document.querySelector('.slider-project');
+// createInitialSlidesProject(initialSlidesProject);
+
+
+
+function createSlideProject(item) {
+  const slideElement = slideProjectTemplate.querySelector('.card-project').cloneNode(true);
+  
+  const elementImage = slideElement.querySelector('.card-project__img');
+  const elementHeading = slideElement.querySelector('.card-project__heading');
+  const elementText = slideElement.querySelector('.card-project__text');
+  const elementLink = slideElement.querySelector('.card-project__button');
+
+  elementImage.setAttribute('src', item.image);
+  elementImage.setAttribute('alt', item.heading);
+  elementHeading.textContent = item.heading;
+  elementText.textContent = item.text;
+  elementLink.setAttribute('href', item.link);
+
+  return slideElement;
+}
+
+function addSlideProject (item, container) {
+  const slideElement = createSlideProject(item);
+  container.append(slideElement);
+}
+
+function createInitialSlidesProject(slides) {
+  slides.forEach((item) => {
+    addSlideProject(item, sliderProjectContainer);
+  })
+}
+
+createInitialSlidesProject(initialSlidesProject);
+const slidesSpecPrjct = document.querySelectorAll('.card-project');
+
+let slideSpecPrjctInd = 1;
+showSlideSpecPrjct(slideSpecPrjctInd);
+
+function showNextSlideSpecPrjct() {
+  showSlideSpecPrjct(slideSpecPrjctInd += 1);
+}
+
+function showPrevSlideSpecPrjct() {
+  showSlideSpecPrjct(slideSpecPrjctInd -= 1);
+}
+
+ function hideSlideSpecPrjct() {
+  slidesSpecPrjct.forEach((item) => {
+    if (item.classList.contains('card-project_visible')) {
+      item.classList.remove('card-project_visible');
+    }
+  });
+ }
+
+function showSlideSpecPrjct(n) {
+  if (n > slidesSpecPrjct.length) {
+    slideSpecPrjctInd = 1;
+  }
+
+  if (n < 1) {
+    slideSpecPrjctInd = slidesSpecPrjct.length;
+  }
+
+  hideSlideSpecPrjct();
+
+  slidesSpecPrjct[slideSpecPrjctInd - 1].classList.add('card-project_visible');
+}
+
+buttonPrevSliderPrct.addEventListener('click', showPrevSlideSpecPrjct);
+buttonNextSliderPrct.addEventListener('click', showNextSlideSpecPrjct);
