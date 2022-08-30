2aac2f0dcfdedebcb7550313d2c0d3f514230418
Aleksandra Shumkova <shumkaleksandra@yandex.ru>
2022-08-30 20:55:19 +0700

    fix: удаляет template для слайда спецпроектов

--- script/script-slider-project.js
+++ script/script-slider-project.js
@@ -1,45 +1,5 @@
-const slideProjectTemplate = document.querySelector('#slide-for-project').content;
 const buttonPrevSliderPrct = document.querySelector('.slider-project__button_type_prev');
 const buttonNextSliderPrct = document.querySelector('.slider-project__button_type_next');
-
-const sliderProjectContainer = document.querySelector('.slider-project');
-
-const createPictureEl = (imgName, imgAlt, imgClass) => {
-  return `<picture>
-            <source type="image/webp" srcset="images/${imgName}@1x.webp 1x, images/${imgName}@2x.webp 2x">
-            <img class=${imgClass} alt="${imgAlt}." src="images/${imgName}@1x.jpg" srcset="images/${imgName}@2x.jpg 2x"/>
-          </picture>`;
-}
-
-function createSlideProject(item) {
-  const slideElement = slideProjectTemplate.querySelector('.card-project').cloneNode(true);
-
-  const elementImageWrapper = slideElement.querySelector('.card-project__img-wrapper');
-  const elementHeading = slideElement.querySelector('.card-project__heading');
-  const elementText = slideElement.querySelector('.card-project__text');
-  const elementLink = slideElement.querySelector('.card-project__button');
-
-  elementImageWrapper.innerHTML = createPictureEl(item.image, item.heading, 'card-project__img')
-
-  elementHeading.textContent = item.heading;
-  elementText.textContent = item.text;
-  elementLink.setAttribute('href', item.link);
-
-  return slideElement;
-}
-
-function addSlideProject (item, container) {
-  const slideElement = createSlideProject(item);
-  container.append(slideElement);
-}
-
-function createInitialSlidesProject(slides) {
-  slides.forEach((item) => {
-    addSlideProject(item, sliderProjectContainer);
-  })
-}
-
-createInitialSlidesProject(initialSlidesProject);
 const slidesSpecPrjct = document.querySelectorAll('.card-project');
 
 let slideSpecPrjctInd = 1;
